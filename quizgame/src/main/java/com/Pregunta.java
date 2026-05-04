baa8c34c394042625a55aa7bc3069f06d8052394
Durluc1324 <lucianorodriguezpaven@gmail.com>
2026-05-04 16:54:57 -0300

    creacion de clase pregunta

---
+++ quizgame/src/main/java/com/Pregunta.java
@@ -0,0 +1,52 @@
+package com;
+
+import com.ucu.edu.aed.impl.ListaArrayList;
+import com.ucu.edu.aed.impl.ListaArrayList;
+
+public class Pregunta {
+    private int idPregunta;
+    private String enunciado;
+    private ListaArrayList<String> opciones;
+    private String respuestaCorrecta;
+    private String categoria;
+    private boolean esCorrecta;
+
+    public Pregunta(int idPregunta, String enunciado, String opciones, String respuestaCorrecta, String categoria) {
+        this.idPregunta = idPregunta;
+        this.enunciado = enunciado;
+        this.respuestaCorrecta = respuestaCorrecta;
+        this.categoria = categoria;
+        this.opciones = new ListaArrayList<>();
+
+       
+        String[] partes = opciones.split(",");
+        for (String opcion : partes) {
+            this.opciones.agregar(opcion.trim());
+        }
+}
+
+    public int getId() {
+        return this.idPregunta;
+    }
+
+    public String getEnunciado(){
+        return this.enunciado;
+    }
+
+    public String getRespuestaCorrecta(){
+        return this.respuestaCorrecta;
+    }
+
+    public boolean getEsCorrecta(){
+        return this.esCorrecta;
+    }
+
+    public ListaArrayList<String> getOpciones(){
+        return opciones;
+    }
+
+    public String getCategoria(){
+        return this.categoria;
+    }
+
+}
